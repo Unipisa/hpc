ec50430c2fde4521b464453afa1b203182de6635
Guido <guido.scatena@unipi.it>
2020-01-14 12:13:43 +0100

    c

--- source/conf.py
+++ source/conf.py
@@ -27,8 +27,8 @@ author = 'Fabio Pratelli, Guido Scatena'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = ['rst2pdf.pdfbuilder']
+pdf_documents = [('index', u'Unipi HPC Resources Documentation', u'Unipi HPC Resources Documentation', u'Fabio Pratelli, Guido Scatena'),]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
